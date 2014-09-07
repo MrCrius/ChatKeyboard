cdd9eaef8087b9f6174fb9b88255cc215ef63009
Crius <zhoukaifeng1989@126.com>
2014-09-07 19:10:27 +0800

    add

---
+++ ChatKeyboard/ChatKeyboard/views/inputBox/InputTextView.h
@@ -0,0 +1,48 @@
+//
+//  InputTextView.h
+//  ChatKeyboard
+//
+//  Created by 周彬 on 14-9-7.
+//  Copyright (c) 2014年 zhoubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface InputTextView : UITextView
+
+/**
+ *  提示用户输入的标语
+ **/
+@property (nonatomic,strong) NSString *placeHolder;
+
+/**
+ *  标语文本的颜色
+ **/
+@property (nonatomic,strong) UIColor *placeHolderTextColor;
+
+/**
+ *  获取自身文本占据有多少行
+ *
+ *  @return 返回行数
+ */
+- (NSUInteger)numberOfLinesOfText;
+
+/**
+ *  获取每行的高度
+ *
+ *  @return 根据iPhone或者iPad来获取每行字体的高度
+ */
++ (NSUInteger)maxCharactersPerLine;
+
+/**
+ *  获取某个文本占据自身适应宽带的行数
+ *
+ *  @param text 目标文本
+ *
+ *  @return 返回占据行数
+ */
++ (NSUInteger)numberOfLinesForMessage:(NSString *)text;
+
+@end
