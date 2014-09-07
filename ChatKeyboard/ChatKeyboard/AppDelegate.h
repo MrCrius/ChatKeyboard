96acf8b3f4996823a324520510554c8a6478a598
Crius <zhoukaifeng1989@126.com>
2014-09-07 09:47:51 +0800

    create project
    create project

---
+++ ChatKeyboard/ChatKeyboard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChatKeyboard
+//
+//  Created by 周彬 on 14-9-7.
+//  Copyright (c) 2014年 zhoubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
