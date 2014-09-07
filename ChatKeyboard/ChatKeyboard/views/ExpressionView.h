4c601f7588e6d9763a9fbc16698af823a495143c
Crius <zhoukaifeng1989@126.com>
2014-09-07 17:55:26 +0800

    add

---
+++ ChatKeyboard/ChatKeyboard/views/ExpressionView.h
@@ -0,0 +1,13 @@
+//
+//  ExpressionView.h
+//  ChatKeyboard
+//
+//  Created by 周彬 on 14-9-7.
+//  Copyright (c) 2014年 zhoubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpressionView : UIView
+
+@end
